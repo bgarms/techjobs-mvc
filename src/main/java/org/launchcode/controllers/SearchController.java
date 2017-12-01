98492b6511771f41c3d891bc2629292dc1b58e5c
BGarms <garms.brian@gmail.com>
2017-11-30 19:55:25 -0600

    Assignment Complete

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.HashMap;
  */
 @Controller
 @RequestMapping("search")
+//  Request Path: /search
 public class SearchController {
 
     @RequestMapping(value = "")
@@ -21,7 +23,43 @@ public class SearchController {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
+    @RequestMapping(value = "results", method = RequestMethod.GET)
 
-    // TODO #1 - Create handler to process search request and display results
+    // Request Path: /search/results
 
+    public String search (Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        //If Search Type is All --- jobs is created and all of the data in JobData
+        // is sent to web.
+
+        if (searchType.equals("all")){
+            jobs = JobData.findAll();
+
+            if(searchTerm != ""){
+
+                // Will send all data containing searchTerm to the web.
+
+                jobs = JobData.findByValue(searchTerm);
+            }
+
+            // Otherwise, jobs is created with the data based on searchType and searchTerm
+            // and sent to the web.
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("title", "Results");
+        model.addAttribute("searchInfo", jobs);
+
+
+
+        return "search";
+
+    }
 }
+
